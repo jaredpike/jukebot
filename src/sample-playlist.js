4485568170c5b1f6ae08bc375fed1a61c587fd28
jaredpike <jaredpike92@gmail.com>
2017-08-07 23:42:36 -0400

    more samples

---
+++ src/sample-playlist.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        title: 'Krewella - Team',
+        id: '76jARSWqcdM',
+        channel: 'KrewellaVEVO',
+        length: '4:30'
+    }
+];
--- src/sample-results.js
+++ src/sample-results.js
@@ -1,20 +1,32 @@
-module.exports = {
-    result1: {
+module.exports = [
+    {
         title: 'Krewella - Team',
         id: '76jARSWqcdM',
         channel: 'KrewellaVEVO',
         length: '4:30'
     },
-    result2: {
+    {
         title: 'Krewella - Alive',
         id: 'J-gYJBsln-w',
         channel: 'KrewellaVEVO',
         length: '3:44'
     },
-    result3: {
+    {
         title: 'Krewella - We Are One',
         id: 'PKi2giB0aIQ',
         channel: 'KrewellaVEVO',
         length: '3:16'
+    },
+    {
+        title: 'Krewella - Lights and Thunder',
+        id: 'Gwlf_OxIJaA',
+        channel: 'KrewellaVEVO',
+        length: '4:43'
+    },
+    {
+        title: 'Krewella - Be There',
+        id: 'iLU9tY9X5EE',
+        channel: 'KrewellaVEVO',
+        length: '3:26'
     }
-};
+];
