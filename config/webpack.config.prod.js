02284a42f56fd8012462ca393e16bb91440ba426
jaredpike <jaredpike92@gmail.com>
2017-08-20 16:54:54 -0400

    autoprefix for production

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -233,16 +233,33 @@ module.exports = {
       // Remember to add the new extension(s) to the "file" loader exclusion list.
       {
           test: /\.scss$/,
-          use: extractSass.extract({
-              use: [{
-                  loader: "css-loader"
-              }, {
-                  loader: "sass-loader"
-              }],
-              // use style-loader in development
-              fallback: "style-loader"
-          })
-      }
+          use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+          }, {
+              loader: "css-loader" // translates CSS into CommonJS
+          }, {
+              loader: "sass-loader" // compiles Sass to CSS
+          }, {
+              loader: require.resolve('postcss-loader'),
+              options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                      require('postcss-flexbugs-fixes'),
+                      autoprefixer({
+                          browsers: [
+                              '>1%',
+                              'last 4 versions',
+                              'Firefox ESR',
+                              'not ie < 9', // React doesn't support IE8 anyway
+                          ],
+                          flexbox: 'no-2009',
+                      }),
+                  ],
+              },
+          },]
+      },
     ],
   },
   plugins: [
