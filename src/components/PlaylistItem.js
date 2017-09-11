74f441c628a77b4c4839e33377b34c7c0816a9b1
jaredpike <jaredpike92@gmail.com>
2017-09-10 23:06:09 -0400

    restore count

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -4,8 +4,10 @@ import XIcon from './vectors/XIcon';
 class PlaylistItem extends Component {
     render() {
         const { details, index } = this.props;
+        const count = parseInt(index, 10) + 1;
         return (
             <li className="playlist__item">
+                <div className="playlist__order">{count}</div>
                 <h3 className="playlist__title" onClick={() => this.props.playSong(details, index)}>{details.title}</h3>
                 <button className="playlist__icon" onClick={() => this.props.removeFromPlaylist(index)}>
                     <XIcon />
