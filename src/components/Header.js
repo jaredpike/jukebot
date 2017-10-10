7a467f0bd193f6973eff19db89a268c74af73956
jaredpike <jaredpike92@gmail.com>
2017-10-09 23:13:39 -0400

    add robot logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Header.scss';
+import Robot from './vectors/Robot';
 
 const Header = (props) => {
     return (
         <header className="header">
             <div className="header__section">
-                <a className="header__logo">{props.name}</a>
+                <a className="header__logo">
+                    <span><Robot /></span>
+                    <span>{props.name}</span>
+                </a>
             </div>
         </header>
     )
